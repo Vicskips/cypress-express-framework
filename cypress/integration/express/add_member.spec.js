3991bc17a4410e028ecdb6b58e8293b2fdca3682
vicskips <vicskips@gmail.com>
2020-05-16 22:28:29 +0100

    added chance for random names and emails

--- cypress/integration/express/add_member.spec.js
+++ cypress/integration/express/add_member.spec.js
@@ -1,8 +1,20 @@
+const Chance = require("chance");
+
+const ch = new Chance();
+
+function getName() {
+  return ch.first() + " " + ch.last();
+}
+function getEmail() {
+  return ch.email();
+}
+
 describe("adding a member", () => {
-  it("star wars url inside first result", () => {
+  it("adding a members name and email", () => {
     cy.visit("http://localhost:5678/");
-    cy.get("input[name=name]").type("Spike Spiegel");
-    cy.get("input[name=email]").type("spike@spiegel.com");
+
+    cy.get("input[name=name]").type(getName());
+    cy.get("input[name=email]").type(getEmail());
     cy.get("input[type=submit]").click();
   });
 });
